85cc074b0c3e0bd9cf61815c905624b0c1d70186
mkizka <30231179+mkizka@users.noreply.github.com>
2020-09-01 17:34:29 +0900

    更新

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,6 @@
 import os
+import re
+import tempfile
 import subprocess
 import discord
 
@@ -12,18 +14,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message) -> None:
-    suffix = '!py\n'
     if message.author.bot:
         return
-    if message.content.startswith(suffix):
-        program = message.content[len(suffix):]
-        process = subprocess.run(
-            ['python', '-c', f'{program}'],
-            timeout=5,
-            encoding='utf-8',
-            stdout=subprocess.PIPE
-        )
-        await message.channel.send(process.stdout.strip())
+    match = re.match(r'!py\s```py\n(.+)\n```', message.content, flags=(re.MULTILINE | re.DOTALL))
+    if match:
+        program = match.group(1)
+        filepath = tempfile.mktemp(suffix='.py')
+        with open(filepath, 'w') as f:
+            f.write(program)
+        try:
+            process = subprocess.run(
+                ['python', f'{filepath}'],
+                timeout=5,
+                encoding='utf-8',
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            result = f'```\n{process.stderr or process.stdout}\n```'
+        except Exception as e:
+            result = f'実行に失敗しました\n'
+            result += f'入力\n'
+            result += f'```py{program}```\n'
+            result += f'エラー\n'
+            result += f'```{e}```'
+        await message.channel.send(result)
 
 
 TOKEN = os.environ['TOKEN']
